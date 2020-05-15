9c3d0b11115f31e321b63b05c10b151fafe2e1bd
Jordan Last <jordan.michael.last@gmail.com>
2020-05-15 16:12:29 -0600

    add wbtc

--- elements/be-app.ts
+++ elements/be-app.ts
@@ -1,8 +1,10 @@
 import { html, render as litRender } from 'lit-html';
 import { createObjectStore } from 'reduxular';
+import { ethers } from 'ethers';
+import { BigNumber } from 'bignumber.js';
 
 type State = {
-    wbtcTotalSupply: number | 'NOT_SET';
+    wbtcTotalSupply: BigNumber | 'NOT_SET';
 };
 
 const InitialState: Readonly<State> = {
@@ -16,6 +18,13 @@ class BEApp extends HTMLElement {
         super();
 
         // TODO load the balances
+
+        const provider: Readonly<ethers.providers.BaseProvider> = ethers.getDefaultProvider('homestead');
+
+        (async () => {
+            const wbtcTotalSupply = await getWBTCTotalSupply(provider);
+            this.store.wbtcTotalSupply = wbtcTotalSupply.div(10 ** 8);
+        })();
     }
 
     render(state: Readonly<State>) {
@@ -24,9 +33,26 @@ class BEApp extends HTMLElement {
 
             <br>
 
-            <div>wBTC: ${state.wbtcTotalSupply}</div>
+            <div>Total: ${state.wbtcTotalSupply}</div>
+
+            <br>
+
+            <div>wBTC: ${state.wbtcTotalSupply === 'NOT_SET' ? 'Loading...' : state.wbtcTotalSupply}</div>
         `;
     }
 }
 
 window.customElements.define('be-app', BEApp);
+
+// TODO be very careful with numbers
+async function getWBTCTotalSupply(provider: Readonly<ethers.providers.BaseProvider>): Promise<BigNumber> {
+    const wbtcContractAddress = '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599';
+
+    const wbtcABI = [
+        'function totalSupply() public view returns (uint256)'
+    ];
+
+    const wbtcContract = new ethers.Contract(wbtcContractAddress, wbtcABI, provider);
+
+    return await wbtcContract.totalSupply();
+}
