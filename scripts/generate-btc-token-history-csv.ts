0fb0939c89f5675acf91e422b2f5f3af30c665e2
Jordan Last <jordan.michael.last@gmail.com>
2020-06-26 16:31:26 -0600

    change csv to date

--- scripts/generate-btc-token-history-csv.ts
+++ scripts/generate-btc-token-history-csv.ts
@@ -9,34 +9,34 @@ import { sbtcHistory } from '../services/sbtc-history';
 import { tbtcHistory } from '../services/tbtc-history';
 import { wbtcHistory } from '../services/wbtc-history';
 
-const csvColumns = `token,block,timestamp,decimals,amount\n`;
+const csvColumns = `token,date,decimals,amount\n`;
 
 const hbtcRows = hbtcHistory.reduce((result, item) => {
-    return result + `HBTC,${item.blockNumber},${item.timestamp},18,${item.amount}\n`;
+    return result + `HBTC,${new Date(item.timestamp).toISOString()},18,${item.amount}\n`;
 }, '');
 
 const imbtcRows = imbtcHistory.reduce((result, item) => {
-    return result + `imBTC,${item.blockNumber},${item.timestamp},8,${item.amount}\n`;
+    return result + `imBTC,${new Date(item.timestamp).toISOString()},8,${item.amount}\n`;
 }, '');
 
 const pbtcRows = pbtcHistory.reduce((result, item) => {
-    return result + `pBTC,${item.blockNumber},${item.timestamp},18,${item.amount}\n`;
+    return result + `pBTC,${new Date(item.timestamp).toISOString()},18,${item.amount}\n`;
 }, '');
 
 const renbtcRows = renbtcHistory.reduce((result, item) => {
-    return result + `pBTC,${item.blockNumber},${item.timestamp},8,${item.amount}\n`;
+    return result + `pBTC,${new Date(item.timestamp).toISOString()},8,${item.amount}\n`;
 }, '');
 
 const sbtcRows = sbtcHistory.reduce((result, item) => {
-    return result + `renBTC,${item.blockNumber},${item.timestamp},18,${item.amount}\n`;
+    return result + `renBTC,${new Date(item.timestamp).toISOString()},18,${item.amount}\n`;
 }, '');
 
 const tbtcRows = tbtcHistory.reduce((result, item) => {
-    return result + `TBTC,${item.blockNumber},${item.timestamp},18,${item.amount}\n`;
+    return result + `TBTC,${new Date(item.timestamp).toISOString()},18,${item.amount}\n`;
 }, '');
 
 const wbtcRows = wbtcHistory.reduce((result, item) => {
-    return result + `WBTC,${item.blockNumber},${item.timestamp},8,${item.amount}\n`;
+    return result + `WBTC,${new Date(item.timestamp).toISOString()},8,${item.amount}\n`;
 }, '');
 
 const csv = `${csvColumns}${hbtcRows}${imbtcRows}${pbtcRows}${renbtcRows}${sbtcRows}${tbtcRows}${wbtcRows}`;
